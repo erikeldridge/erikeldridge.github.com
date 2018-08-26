2976016447c56304a716a8ed13a2218679a84b23
Erik Eldridge <erikeldridge@gmail.com>
2018-08-26 23:44:11 +0000

    Build index.js using browserify in prep for lunr dependency

---
+++ src/main.js
@@ -0,0 +1,16 @@
+var $ = require('jquery')
+
+var $input = $('#filter')
+var $items = $('li')
+$input.keyup(function(e){
+  var pattern = new RegExp(e.target.value, 'i')
+  $items.each(function(){
+    var $item = $(this)
+    if (pattern.test($item.text())) {
+      $item.show()
+    } else {
+      $item.hide()
+    }
+  })
+})
+$input.show().focus()
